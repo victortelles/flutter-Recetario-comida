91c49ce3608c8e75bdcd8b33979e3b8e5c2ca1c3
victortelles <victor.telles@iteso.mx>
2025-03-05 17:25:49 -0600

    Ajustes en el widget de categorias, (degradados)

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -3,11 +3,13 @@ import 'package:tarea_10_recetario/models/category.dart';
 
 //Constructor
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({
-    super.key,
-    required this.category,
-    required this.onSelectCategory
-    });
+  const CategoryGridItem(
+      {
+        super.key,
+        required this.category,
+        required this.onSelectCategory
+      }
+  );
 
   //informacion que obtiene (titulo, color)
   final Category category;
@@ -26,27 +28,27 @@ class CategoryGridItem extends StatelessWidget {
         decoration: BoxDecoration(
           //Degradado al contenedor
           gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.6),
-              category.color.withOpacity(0.9)
-            ],
-            //Inicia / finaliza (Arriba izq - Abajo Der.)
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-          ),
-      
+              colors: [
+                category.color.withOpacity(0.6),
+                category.color.withOpacity(0.9)
+              ],
+              //Inicia / finaliza (Arriba izq - Abajo Der.)
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+
           //Color y bordeado
           color: category.color,
           borderRadius: BorderRadius.circular(15),
         ),
-      
+
         //Titulo
         alignment: Alignment.topLeft,
         child: Text(
           category.title,
           style: Theme.of(context)
-            .textTheme.titleLarge
-            ?.copyWith(color: Colors.white),
+              .textTheme
+              .titleLarge
+              ?.copyWith(color: Colors.white),
         ),
       ),
     );
