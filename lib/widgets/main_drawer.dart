f0bf0617817045de98aa0c96f54128e760236ddc
victortelles <victor.telles@iteso.mx>
2025-03-05 17:25:00 -0600

    Widget drawer (Siderbar) mostrando las mismas opciones de bottomNav.

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,3 +1,51 @@
-//Sidebar con las opcion hamburgesa de (Categorias y favoritos)
+import 'package:flutter/material.dart';
 
-//Permite navegar de categories screen o MealsScreen
+class MainDrawer extends StatelessWidget {
+  //Funcion que recibe para cambiar pantalla
+  final Function(int) onSelectScreen;
+
+  const MainDrawer({super.key, required this.onSelectScreen});
+
+  //Constructor para ListTile + navegacion
+  Widget buildListTile(String title, IconData icon, VoidCallback tapHandler) {
+    //Widget ListTile (Icono + Titulo)
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(
+        title,
+        style: const TextStyle(fontSize: 24),
+      ),
+      onTap: tapHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Drawer
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            //Titulo menu hamburgesa
+            title: const Text('Recetario de Cocina'),
+            //icono hamburgesa false
+            automaticallyImplyLeading: false,
+          ),
+
+          //Lista 1
+          buildListTile('Categories', Icons.set_meal, () {
+            Navigator.pop(context);
+            onSelectScreen(0);
+          }),
+
+          //Lista 2
+          buildListTile('Your Favorites', Icons.star, () {
+            Navigator.pop(context);
+            onSelectScreen(1);
+          }),
+        ],
+      ),
+    );
+  }
+}
+
