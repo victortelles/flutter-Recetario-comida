3f35942c1fb85837b7dae8be903f7430c953948a
victortelles <victor.telles@iteso.mx>
2025-03-05 23:23:17 -0600

    Rectangulo debajo del widget de meals_item

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,2 +1,31 @@
-//Representa  un platillo de la lista de comidas (MealsScreen)
-//Muestra la imagen de la comida como fondo
+import 'package:flutter/material.dart';
+
+//Widget para auxiliar los iconos y textos
+class MealItemTrait extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const MealItemTrait({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    //Fila
+    return Row(
+      children: [
+        //Icono
+        Icon(icon, color: Colors.white),
+        //Espaciado
+        const SizedBox(width: 8),
+        //Texto
+        Text(
+          label,
+          style: TextStyle(color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
