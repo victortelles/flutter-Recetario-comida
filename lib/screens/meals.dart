71937bb6c6c6e5e1046955e672f9fe59c5603b9f
victortelles <victor.telles@iteso.mx>
2025-03-05 23:24:15 -0600

    Actualizacion pantalla de platillos | Favoritos o Productos

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:tarea_10_recetario/data/dummy_data.dart';
+import 'package:tarea_10_recetario/models/meal.dart';
+import 'package:tarea_10_recetario/widgets/meal_item.dart';
+import 'package:tarea_10_recetario/widgets/meal_item_trait.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  final String categoryId;
+  final String categoryTitle;
+  final List<Meal> favoriteMeals;
+
+  const MealsScreen({
+    super.key,
+    required this.categoryId,
+    required this.categoryTitle,
+    required this.favoriteMeals,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'No hay favoritos aún.',
-        style: TextStyle(
-          fontSize: 24,
-          color: Colors.amber
-          ),
+    //Filtrar los platillos por categoria
+    final filteredMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    //toggle para intercambiar favoritos o categorias
+    final mealsToShow =
+        categoryId == 'favorites' ? favoriteMeals : filteredMeals;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
       ),
+
+      //Si se encuentra vacio
+      body: mealsToShow.isEmpty
+          ? const Center(
+              child: Text(
+                'No hay platillos disponibles.',
+                style: TextStyle(fontSize: 24, color: Colors.amber),
+              ),
+            )
+
+      //Si no esta vacio
+          : ListView.builder(
+              itemCount: mealsToShow.length,
+              itemBuilder: (ctx, index) {
+                return MealItem(
+                  meal: mealsToShow[index],
+                  onSelectMeal: () {
+                    // Aquí se llamará la función para abrir los detalles del platillo
+                  },
+                );
+              },
+            ),
     );
   }
 }
